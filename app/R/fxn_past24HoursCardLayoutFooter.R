70167e588761f2bd531820727be6af76d8eac55a
Jeremy Weiss <jlweiss@email.arizona.edu>
2025-10-24 16:59:48 -0700

    tab footer text edits

--- app/R/fxn_past24HoursCardLayoutFooter.R
+++ app/R/fxn_past24HoursCardLayoutFooter.R
@@ -10,7 +10,7 @@ fxn_past24HoursCardLayoutFooter <- function() {
         paste0(
           "<sup>1</sup> Values based on measurements taken 12 inches above ground&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<sup>2</sup> Latest Update values are from Sensor 1 and Sensor 2, respectively
         
-        <br><br>DC values less than or equal to ", thresholdMeanMVDry, " mV indicate dry conditions, between ", thresholdMeanMVDry, " and ", thresholdMeanMVWet, " mV the transition between dry and wet conditions, and equal to or greater than ", thresholdMeanMVWet, " mV wet conditions. Higher DC values indicate greater leaf wetness. Values of 'NA' denote no data. Variable key: <strong>RH</strong> relative humidity in percent; <strong>T<sub>air</sub></strong> air temperature in degrees Fahrenheit; <strong>T<sub>dew point</sub></strong> dew point temperature in degrees Fahrenheit; <strong>DC</strong> dielectric constant in millivolts, proportional to the amount of water or ice on the upper surface of a leaf wetness sensor"
+        <br><br>Values of 'NA' denote no data. Variable key: <strong>RH</strong> relative humidity in percent; <strong>T<sub>air</sub></strong> air temperature in degrees Fahrenheit; <strong>T<sub>dew point</sub></strong> dew point temperature in degrees Fahrenheit; <strong>DC</strong> dielectric constant in millivolts, proportional to the amount of water or ice on the upper surface of a leaf wetness sensor. DC values less than or equal to ", thresholdMeanMVDry, " mV indicate dry conditions, between ", thresholdMeanMVDry, " and ", thresholdMeanMVWet, " mV the transition between dry and wet conditions, and equal to or greater than ", thresholdMeanMVWet, " mV wet conditions. Higher DC values indicate greater leaf wetness."
         )
       ),
       
